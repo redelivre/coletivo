dd7046195b4980f227c49bb064a7ddcdd0813ddb
Diego Rojas <diego@brasa.art.br>
2016-10-18 21:46:07 -0200

    coloca imagem destacada em full size no background da seção Page Featured e aplica cor branca para contrastar

--- section-parts/section-featuredpage.php
+++ section-parts/section-featuredpage.php
@@ -14,33 +14,29 @@ if ( ! empty( $page_ids ) ) {
     ?>
     <?php if (!$onepress_featuredpage_disable) { ?>
         <?php if ( ! onepress_is_selective_refresh() ){ ?>
-        <section id="<?php if ($onepress_featuredpage_id != '') {
+
+<?php
+if ( ! empty ( $page_ids ) ) {
+global $post;
+$post_id = $page_ids[0];
+$post_id = apply_filters( 'wpml_object_id', $post_id, 'page', true );
+$post = get_post( $post_id );
+setup_postdata( $post );
+?>
+<?php if(has_post_thumbnail($post_id)): ?>
+<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post_id), 'full' );?>
+        <section style="background:url(<?php echo $thumb[0]; ?>) center no-repeat;     background-size: 100%;" id="<?php if ($onepress_featuredpage_id != '') {
             echo $onepress_featuredpage_id;
         }; ?>" <?php do_action('onepress_section_atts', 'featuredpage'); ?> class="<?php echo esc_attr(apply_filters('onepress_section_class', 'section-featuredpage section-padding onepage-section', 'featuredpage')); ?>">
-        <?php } ?>
-
-            <?php do_action('onepress_section_before_inner', 'featuredpage'); ?>
+    <div class="content"> 
+    <?php else :
+    echo '<div class="section-title-area"><br/><br/><h5 class="section-subtitle">You need to select a Featured Image for a background in full size</h5></div>';
+    endif;?>
+    <?php } ?>
+<?php do_action('onepress_section_before_inner', 'featuredpage'); ?>
             <div class="container">
-                <div class="content">
-                    <?php
-                    if ( ! empty ( $page_ids ) ) {
-                        global $post;
-                            $post_id = $page_ids[0];
-                            $post_id = apply_filters( 'wpml_object_id', $post_id, 'page', true );
-                            $post = get_post( $post_id );
-                            setup_postdata( $post );
-                            ?>
                             <div class="section-title-area">
                                 <h2 class="section-title"><?php the_title(); ?></h2>
-							<div class="list-article-thumb">
-									<?php
-									if ( has_post_thumbnail( ) ) {
-										the_post_thumbnail( 'onepress-blog-small' );
-									} else {
-										echo '( Selecione uma imagem destacada para esta Página, ela vai aparecer aqui. )';
-									}
-									?>
-							</div>
 		                            <div class="section-desc">
 		                              <?php
 		                                if ( $content_source == 'excerpt' ) {
@@ -54,7 +50,7 @@ if ( ! empty( $page_ids ) ) {
 		                        <a href="<?php echo esc_url( get_permalink()) ;?>" class="btn btn-theme-primary btn-lg">
 									<?php _e('Acesse','onepress');?>
 								</a>
-		                    </div>      
+		                    </div>
                             <?php
                         wp_reset_postdata();
                     }// ! empty pages ids
