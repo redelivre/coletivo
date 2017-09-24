29ba0c6cf504a429659aba9301549b8ae542b2fe
everaldomatias <matiaseveraldo@gmail.com>
2017-09-24 20:50:57 -0300

    Coding standards in footer.php

--- footer.php
+++ footer.php
@@ -10,33 +10,34 @@
  */
 ?>
     <footer id="colophon" class="site-footer" role="contentinfo">
-        <?php $coletivo_btt_disable = get_theme_mod('coletivo_btt_disable'); ?>
+        <?php $coletivo_btt_disable = get_theme_mod( 'coletivo_btt_disable' ); ?>
         <div class="site-info">
             <div class="container">
-                <?php if ($coletivo_btt_disable != '1') : ?>
+                <?php if ( $coletivo_btt_disable != '1' ) : ?>
                     <div class="btt">
                         <a class="back-top-top" href="#page" title="<?php echo esc_html__('Back To Top', 'coletivo') ?>"><i class="fa fa-angle-double-up wow flash" data-wow-duration="2s"></i></a>
-                    </div>
+                    </div><!-- btt -->
                 <?php endif; ?>
+                
                 <?php
-                /**
-                 * hooked coletivo_footer_site_info
-                 * @see coletivo_footer_site_info
-                 */
-                do_action('coletivo_footer_site_info');
+                    /**
+                     * Hooked: coletivo_footer_site_info
+                     *
+                     * @see coletivo_footer_site_info
+                     */
+                    do_action( 'coletivo_footer_site_info' );
                 ?>
-            </div>
-        </div>
-        <!-- .site-info -->
+            </div><!-- container -->
+        </div><!-- .site-info -->
     </footer><!-- #colophon -->
-<?php
-/**
- * Hooked: coletivo_site_footer
- *
- * @see coletivo_site_footer
- */
-do_action( 'coletivo_site_end' );
-?>
+    <?php
+        /**
+         * Hooked: coletivo_site_footer
+         *
+         * @see coletivo_site_footer
+         */
+        do_action( 'coletivo_site_end' );
+    ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
