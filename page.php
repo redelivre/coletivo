99edf293f79087559f0e0b078accbd49738dbb67
everaldomatias <matiaseveraldo@gmail.com>
2017-10-02 21:55:37 -0300

    Coding standards page.php

--- page.php
+++ page.php
@@ -15,15 +15,20 @@
 get_header(); ?>
 
 	<div id="content" class="site-content">
-<?php if(has_post_thumbnail() && $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full', true )):?>
-		<div class="page-fullheader">
-		 <img src="<?php echo $img[0];?>"/>
-		</div>
-		<?php endif;?>
+		
+		<?php if( has_post_thumbnail() && $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full', true ) ) : ?>
+			<div class="page-fullheader">
+				<img src="<?php echo esc_url( $img[0] ); ?>" alt="<?php the_title(); ?>">
+			</div><!-- page-fullheader -->
+		<?php endif; ?>
+		
 		<div id="content-inside" class="container no-sidebar">
-		<?php the_title( '<h2 class="fullheader-title">', '</h2>' ); ?>
+		
+			<?php the_title( '<h2 class="fullheader-title">', '</h2>' ); ?>
+
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
+					
 					<?php while ( have_posts() ) : the_post(); ?>
 
 						<?php get_template_part( 'template-parts/content', 'page' ); ?>
