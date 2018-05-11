4524f0dd14e8ff2d909eba0bfb7b61661a48d329
Diego Rojas <diego@brasa.art.br>
2018-05-11 09:14:03 -0300

    adicionado template fullwidth para facilitar a vida dos builders nas paginas internas

---
+++ template-fullwidth.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ *Template Name: Full Width
+ *
+ * @package coletivo
+ */
+
+get_header(); ?>
+
+	<div id="content" class="container">
+			<?php the_title( '<h2 class="fullheader-title">', '</h2>' ); ?>
+			<div id="fullwidth" class="content-area">
+					
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
+
+					<?php endwhile; // End of the loop. ?>
+
+			</div><!-- #fullwidth -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
