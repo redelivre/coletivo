c97c1bc3f4f7516efecb1e2144e4ec8cd051fe74
everaldomatias <matiaseveraldo@gmail.com>
2018-05-23 20:28:00 -0300

    Coding standards no arquivo page.php

--- page.php
+++ page.php
@@ -14,37 +14,36 @@
 
 get_header(); ?>
 
-	<div id="content" class="site-content">
-		
-		<?php if( has_post_thumbnail() && $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full', true ) ) : ?>
-			<div class="page-fullheader">
-				<img src="<?php echo esc_url( $img[0] ); ?>" alt="<?php the_title(); ?>">
-			</div><!-- page-fullheader -->
-		<?php endif; ?>
-		
-		<div id="content-inside" class="container no-sidebar">
-		
-			<?php the_title( '<h2 class="fullheader-title">', '</h2>' ); ?>
+<div id="content" class="site-content">
+	
+	<?php if ( has_post_thumbnail() && $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full', true ) ) : ?>
+		<div class="page-fullheader">
+			<img src="<?php echo esc_url( $img[0] ); ?>" alt="<?php the_title(); ?>">
+		</div><!-- .page-fullheader -->
+	<?php endif; ?>
+	
+	<div id="content-inside" class="container no-sidebar">
+	
+		<?php the_title( '<h2 class="fullheader-title">', '</h2>' ); ?>
 
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
-					
-					<?php while ( have_posts() ) : the_post(); ?>
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/content', 'page' );
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				endwhile; // End of the loop.
+				?>
 
-						<?php
-							// If comments are open or we have at least one comment, load up the comment template.
-							if ( comments_open() || get_comments_number() ) :
-								comments_template();
-							endif;
-						?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-					<?php endwhile; // End of the loop. ?>
+	</div><!--#content-inside -->
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
-		</div><!--#content-inside -->
-	</div><!-- #content -->
+</div><!-- #content -->
 
-<?php get_footer(); ?>
+<?php get_footer();
