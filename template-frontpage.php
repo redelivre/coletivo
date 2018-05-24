514751f6db4c4f703e4b4ede6a175cc107214f92
everaldomatias <matiaseveraldo@gmail.com>
2018-05-23 21:05:31 -0300

    Coding standards no arquivo template-frontpage.php

--- template-frontpage.php
+++ template-frontpage.php
@@ -1,54 +1,54 @@
 <?php
 /**
- *Template Name: Frontpage
+ * Template Name: Frontpage
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/page-template-files/
  *
  * @package coletivo
  */
 
 get_header(); ?>
 
-	<div id="content" class="site-content">
-		<main id="main" class="site-main" role="main">
-            <?php
+<div id="content" class="site-content">
+	<main id="main" class="site-main" role="main">
 
-            do_action( 'coletivo_frontpage_before_section_parts' );
+        <?php
+        do_action( 'coletivo_frontpage_before_section_parts' );
 
-			if ( ! has_action( 'coletivo_frontpage_section_parts' ) ) {
+		if ( ! has_action( 'coletivo_frontpage_section_parts' ) ) {
+            
+            $order = get_theme_mod( 'coletivo_sections_order', 'hero,features,yourslider,featuredpage,services,portfolio,videolightbox,gallery,team,news,contact,social' );
+            $order = explode( ',', $order );
+            $sections = apply_filters( 'coletivo_frontpage_sections_order', $order );
+
+			foreach ( $sections as $section ) {
                 
-                $order = get_theme_mod( 'coletivo_sections_order', 'hero,features,yourslider,featuredpage,services,portfolio,videolightbox,gallery,team,news,contact,social' );
-                $order = explode( ',', $order );
-                
-                // var_dump( get_theme_mod( 'coletivo_sections_order' ) );
-                $sections = apply_filters( 'coletivo_frontpage_sections_order',
-                $order );
+                /**
+                 * Hook before section
+                 */
+                do_action( 'coletivo_before_section_' . $section );
+                do_action( 'coletivo_before_section_part', $section );
 
-				foreach ( $sections as $section ){
-                    /**
-                     * Hook before section
-                     */
-                    do_action('coletivo_before_section_'.$section );
-                    do_action( 'coletivo_before_section_part', $section );
+                /**
+                 * Load section template part
+                 */
+				get_template_part( 'section-parts/section', $section );
 
-                    /**
-                     * Load section template part
-                     */
-					get_template_part( 'section-parts/section', $section );
+                /**
+                 * Hook after section
+                 */
+                do_action( 'coletivo_after_section_part', $section );
+                do_action( 'coletivo_after_section_' . $section );
 
-                    /**
-                     * Hook after section
-                     */
-                    do_action('coletivo_after_section_part', $section );
-                    do_action('coletivo_after_section_'.$section );
-				}
-
-			} else {
-				do_action( 'coletivo_frontpage_section_parts' );
 			}
 
-            do_action( 'coletivo_frontpage_after_section_parts' );
+		} else {
+			do_action( 'coletivo_frontpage_section_parts' );
+		}
 
-			?>
-		</main><!-- #main -->
-	</div><!-- #content -->
+        do_action( 'coletivo_frontpage_after_section_parts' );
+		?>
+	</main><!-- #main -->
+</div><!-- #content -->
 
-<?php get_footer(); ?>
+<?php get_footer();
