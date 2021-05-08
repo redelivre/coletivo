1557b95984fe065c934f5e1ca4c3084df570e4a7
rodineicosta <rodineicosta@physistec.com.br>
2021-05-08 11:16:36 -0300

    Alterações mínimas

--- section-parts/section-features.php
+++ section-parts/section-features.php
@@ -5,37 +5,37 @@
  * @package coletivo
  */
 
-$coletivo_featureds_id       = get_theme_mod( 'coletivo_features_id', esc_html__( 'features', 'coletivo' ) );
-$coletivo_featureds_disable  = get_theme_mod( 'coletivo_features_disable' ) === 1 ? true : false;
-$coletivo_featureds_title    = get_theme_mod( 'coletivo_features_title', esc_html__( 'Features', 'coletivo' ) );
-$coletivo_featureds_subtitle = get_theme_mod( 'coletivo_features_subtitle', esc_html__( 'Why choose Us', 'coletivo' ) );
+$coletivo_features_id       = get_theme_mod( 'coletivo_features_id', esc_html__( 'features', 'coletivo' ) );
+$coletivo_features_disable  = get_theme_mod( 'coletivo_features_disable' ) === 1 ? true : false;
+$coletivo_features_title    = get_theme_mod( 'coletivo_features_title', esc_html__( 'Features', 'coletivo' ) );
+$coletivo_features_subtitle = get_theme_mod( 'coletivo_features_subtitle', esc_html__( 'Why choose Us', 'coletivo' ) );
 if ( coletivo_is_selective_refresh() ) {
 	$disable = false;
 }
 $data = coletivo_get_section_features_data();
 if ( ! $disable && ! empty( $data ) ) {
-	$coletivo_featureds_desc = get_theme_mod( 'coletivo_features_desc' );
+	$coletivo_features_desc = get_theme_mod( 'coletivo_features_desc' );
 
 	?>
 	<?php if ( ! coletivo_is_selective_refresh() ) { ?>
-		<section id="<?php echo esc_attr( $coletivo_featureds_id ); ?>" <?php do_action( 'coletivo_section_atts', 'features' ); ?>
+		<section id="<?php echo esc_attr( $coletivo_features_id ); ?>" <?php do_action( 'coletivo_section_atts', 'features' ); ?>
 		class="<?php echo esc_attr( apply_filters( 'coletivo_section_class', 'section-features section-padding section-meta onepage-section', 'features' ) ); ?>">
 	<?php } ?>
 	<?php do_action( 'coletivo_section_before_inner', 'features' ); ?>
 	<div class="container">
-		<?php if ( $coletivo_featureds_title || $coletivo_featureds_subtitle || $coletivo_featureds_desc ) { ?>
+		<?php if ( $coletivo_features_title || $coletivo_features_subtitle || $coletivo_features_desc ) { ?>
 			<div class="section-title-area">
 				<?php
-				if ( '' !== $coletivo_featureds_subtitle ) {
-					echo '<h5 class="section-subtitle">' . esc_html( $coletivo_featureds_subtitle ) . '</h5>';
+				if ( '' !== $coletivo_features_subtitle ) {
+					echo '<h5 class="section-subtitle">' . esc_html( $coletivo_features_subtitle ) . '</h5>';
 				}
 
-				if ( '' !== $coletivo_featureds_title ) {
-					echo '<h2 class="section-title">' . esc_html( $coletivo_featureds_title ) . '</h2>';
+				if ( '' !== $coletivo_features_title ) {
+					echo '<h2 class="section-title">' . esc_html( $coletivo_features_title ) . '</h2>';
 				}
 
-				if ( $coletivo_featureds_desc ) {
-					echo '<div class="section-desc">' . apply_filters( 'the_content', wp_kses_post( $coletivo_featureds_desc ) ) . '</div>'; // phpcs:ignore
+				if ( $coletivo_features_desc ) {
+					echo '<div class="section-desc">' . apply_filters( 'the_content', wp_kses_post( $coletivo_features_desc ) ) . '</div>'; // phpcs:ignore
 				}
 				?>
 			</div>
