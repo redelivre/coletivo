41564e3f7acf78b36123abad8051e1cad6a9e436
rodineicosta <rodineicosta@physistec.com.br>
2021-05-15 19:50:57 -0300

    FIx: Carregamento do Control-Scripts

--- inc/customizer-controls.php
+++ inc/customizer-controls.php
@@ -229,6 +229,22 @@ if ( ! class_exists( 'EditorScripts' ) ) {
 	require_once get_template_directory() . '/inc/classes/EditorScripts.php';
 }
 
-if ( ! class_exists( 'ControlScripts' ) ) {
-	require_once get_template_directory() . '/inc/classes/ControlScripts.php';
+/**
+ * Customizer control scripts
+ *
+ * @since  1.7.0
+ * @access public
+ *
+ * @return void
+ */
+function customizer_control_scripts() {
+	wp_enqueue_media();
+	wp_enqueue_script( 'jquery-ui-sortable' );
+	wp_enqueue_script( 'wp-color-picker' );
+	wp_enqueue_style( 'wp-color-picker' );
+
+	wp_enqueue_script( 'coletivo-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-controls', 'wp-color-picker' ), VERSION, true );
+	wp_enqueue_style( 'coletivo-customizer', get_template_directory_uri() . '/assets/css/customizer.css', false, VERSION );
+	wp_add_inline_script( 'coletivo-customizer', 'coletivo_customizer', array( 'before_section_title' => __( 'Section: ', 'coletivo' ) ) );
 }
+add_action( 'customize_controls_enqueue_scripts', 'customizer_control_scripts', 99 );
